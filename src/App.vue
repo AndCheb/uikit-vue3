2cf0533b811ab3ebe21b6857ef0a577b2e55f8b0
Andrey Cheban <anceb@yandex.ru>
2024-06-03 23:03:30 +0300

    add 'Header' component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup>
+import LayoutHeader from '@/components/layout/Header.vue';
 </script>
 
 <template>
-  <router-view />
+  <div class="container">
+    <LayoutHeader />
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @import './styles/global.scss';
+
+.content {
+  margin-left: 250px;
+  padding: 30px;
+  max-width: 1400px;
+  transition: 0.2s;
+
+  &_full {
+    margin-left: 0;
+  }
+}
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <router-link to="/">UI Kit - VUE3</router-link>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: relative;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  height: 62px;
+  font-weight: 700;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.07);
+}
+</style>
